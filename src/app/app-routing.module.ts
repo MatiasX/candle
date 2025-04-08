a005938040a9f5b524a6d226f55e64f6494e0b46
Fehér Mátyás <waldorfosmatyi@gmail.com>
2025-04-09 01:37:19 +0200

    Second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component';
+import { UnderConstructionComponent } from './under-construction/under-construction.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'fejlesztes-alatt', component: UnderConstructionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
